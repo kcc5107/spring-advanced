04af237720c3200fbc5688abef152b2ddef5b71d
Jang Gyeonghyuk <kcc5107@naver.com>
2025-06-11 19:00:49 +0900

    feat: 레벨 3-1. 테스크 코드 연습

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -3,15 +3,17 @@ package org.example.expert.config;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
-class PasswordEncoderTest {
+public class PasswordEncoderTest {
 
     @InjectMocks
-    private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder = new PasswordEncoder();
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
@@ -20,7 +22,7 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
